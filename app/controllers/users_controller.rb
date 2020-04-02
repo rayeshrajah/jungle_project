8c0d4b6a2721bbf009707624b5b9df8ee277ae1b
Rayesh <rayeshrajah@gmail.com>
2020-04-02 15:43:10 +0000

    modified user controller and created session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
     end
 
     def create
+     user = Users.new(user_params)
+     
+     if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+     else
+        redirect_to '/signup'
+    end
+end
+
+    private def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 end
