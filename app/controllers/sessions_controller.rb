ec95064de9e31e55f9e58c1e1a3b04c7538a9c0c
Rayesh <rayeshrajah@gmail.com>
2020-04-02 16:39:48 +0000

    fixed a bug when logging out user name still appeared

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        session[:id] = nil
+        session[:user_id] = nil
         redirect_to '/login'
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     end
 
     def create
-     user = Users.new(user_params)
+     user = User.new(user_params)
      
      if user.save
         session[:user_id] = user.id
