1cea050b6b954568dc8a8b7178ddd473172e437a
Rayesh <rayeshrajah@gmail.com>
2020-04-06 21:32:14 +0000

    made some adjustments to include the error to the expected error

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Product, type: :model do
     
     it 'is valid with valid attributes' do
       category = Category.new(name: "Some New Category")
-      product = described_class.new(name: "Beenie", price: 12, quantity: 15, category: category)
+      product = described_class.new(name: "Beenie", price_cents: 12, quantity: 15, category: category)
       expect(product).to be_valid
       #expect(product.errors.full_messages).to be_empty
     end
@@ -14,28 +14,28 @@ RSpec.describe Product, type: :model do
     #Validates if the product has a name
     it 'is invalid without a name' do
       category = Category.new(name: "New Category")
-      product = described_class.new(name: nil, price: 13, quantity: 12, category: category)
-      expect(product.name).to be_nil
-      expect(product.errors.full_messages).to be_empty
+      product = described_class.new(name: nil, price_cents: 13, quantity: 12, category: category)
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'is invalid without a price' do
       category = Category.new(name: "Some Category")
-      product = described_class.new(name: "Big Tools", price: nil, quantity: 12, category: category)
-      expect(product.price).to be_nil
-      expect(product.errors.full_messages).to be_empty
+      product = described_class.new(name: "Big Tools", price_cents: nil, quantity: 12, category: category)
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Price can't be blank")
     end
     it 'is invalid without a quantity' do
       category = Category.new(name: "Which category")
-      product = described_class.new(name: "Chemise", price: 30, quantity: nil, category: category)
-      expect(product.quantity).to be_nil
-      expect(product.errors.full_messages).to be_empty
+      product = described_class.new(name: "Chemise", price_cents: 30, quantity: nil, category: category)
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
     it 'is invalid without a category' do
       category = Category.new(name: "Hammers")
-      product = described_class.new(name: "Jack-Hammer", price: 150, quantity: 40, category: nil)
-      expect(product.category).to be_nil
-      expect(product.errors.full_messages).to be_empty
+      product = described_class.new(name: "Jack-Hammer", price_cents: 150, quantity: 40, category: nil)
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
