a4e548b4ed1acb4f0401b68e94b775dd03222f17
Rayesh <rayeshrajah@gmail.com>
2020-04-06 22:47:39 +0000

    fixed the test where any test would fail. Fixed by putting validates attributes in model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- before :each do
- @user = User.new(first_name: 'Rayesh', last_name: 'Rajah', email: 'ray_6785876@hotmail.com', password: "ChickenPouletYeah", password_confirmation: "ChickenPouletYeah")
+ before :each do @user = User.new(first_name: 'Rayesh', last_name: 'Rajah', email: 'ray_6785876@hotmail.com', password: "ChickenPouletYeah", password_confirmation: "ChickenPouletYeah")
  end  
 
   describe "Validation" do
@@ -11,7 +10,15 @@ RSpec.describe User, type: :model do
     end
 
     it 'is not valid without a first name' do
-      expect(@user).to be_valid
+     @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+    it 'is not valid without a last name' do
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+    it 'is not valid without an email' do
+      @user.email = nil
     end
   end
 end
