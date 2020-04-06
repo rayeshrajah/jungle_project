4a70dea5e702b8e1a0faf3047b47d971470d6661
Rayesh <rayeshrajah@gmail.com>
2020-04-06 22:52:28 +0000

    Added password test if empty is invalid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,9 @@ RSpec.describe User, type: :model do
       @user.email = nil
       expect(@user).to_not be_valid
     end
+    it 'is not valid without a password' do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
   end
 end
