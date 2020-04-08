a9fb32d456f9fffe84dedf1e77ab2c059a38177a
Rayesh <rayeshrajah@gmail.com>
2020-04-08 22:35:37 +0000

    Added a bug where the loggin and signup didnt work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,5 @@
 class SessionsController < ApplicationController
-    def new
-    end
-
     def  create
-     #user = User.find_by_email(params[:email])
      if user = User.authenticate_with_credentials(params[:email], params[:password])
         session[:user_id] = user.id 
         redirect_to '/'
@@ -11,7 +7,6 @@ class SessionsController < ApplicationController
         redirect_to '/login'
      end
     end
-
     def destroy
         session[:user_id] = nil
         redirect_to '/login'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 
     def create
      user = User.new(user_params)
-     
      if user.save
         session[:user_id] = user.id
         redirect_to '/'
@@ -12,8 +11,7 @@ class UsersController < ApplicationController
         redirect_to '/signup'
     end
 end
-
-    private def user_params
+     def user_params
         params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
